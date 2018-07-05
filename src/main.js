1510d5edcbf15af934e9764d499b040a6311d016
AlidaContreras <alidacontreras01@gmail.com>
2018-07-05 08:27:37 -0500

    ya esta el select de sedes

--- src/main.js
+++ src/main.js
@@ -1,18 +1,29 @@
 //vamos a declarar una variable por cada url para jarlarlas
+const urlCohorts = '../data/cohorts.json';
 const urlUsers = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
 const urlProgress = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
-const urlCohorts = '../data/cohorts.json';
-const listCohorts = document.getElementById('cohorts');
-//llamado de datos
-const xhr = new XMLHttpRequest();
-xhr.open('GET', urlCohorts);
-xhr.onload = getCohorts = (urlCohorts) => {
-	const data = JSON.parse(event.target.responseText);
-	console.log(data);
-};
-xhr.onerror = handleError = () => {
-	console.log('ocurrio un error');
-};
-xhr.send();
-
-
+// DOM
+const selectCohort=document.getElementById('selectCohort');
+const selectSede=document.getElementById('selectSede');
+//llamando a los Cohorts
+const getDataJson=(url,callback)=>{
+const request = new XMLHttpRequest();
+request.open('GET', callback);
+request.onload = callback;
+request.onerror= handleError;
+cohortsRequest.send();
+}
+cohortsRequest.onerror = handleError = () => {
+	console.log('Ocurrió un error');
+}
+//funcion para seleccionar cohort segun sede
+const cohorts=(event)=>{
+	let cohortsfilter='';
+	const data =JSON.parse(event.target.responseText);
+	for (const value of data) {
+		if(((data[i].id).includes(selectSede.value,0)===true)){
+			cohortsfilter += '<option>' + data[i].id + '</option>' + '<br>'
+		}
+	}
+	selectCohort.innerHTML=cohortsfilter;
+}
