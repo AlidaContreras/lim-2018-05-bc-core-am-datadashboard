7c41fc9b0a9db85012a0348f170222ff65f17aaa
AlidaContreras <alidacontreras01@gmail.com>
2018-07-07 22:45:54 -0500

    ya filtra los cohort por sede :)

--- src/main.js
+++ src/main.js
@@ -5,43 +5,68 @@ const urlProgress = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
 const selectSede = document.getElementById('selectSede');
 const inputName = document.getElementById('inputName');
 
+
 // const arraySedes = ['LIM', 'GDL','AQP','SCL','SPL','CDMX'];
 //_______________________________PARA FILTRAR SEGUN SEDE LIM__________________________________________________//
-// if ((idCohort).includes('lim', 0) === true) {
+// 	const arrayDeCohorts = cohorts.filter((elementCohorts) => {
+// 	let idCohort = elementCohorts.id ;
+//	if ((idCohort).includes('lim', 0) === true) {
 // 	//aqui tengo que poner en vez de lim, la etiqueta que va a ser seleccionada en el select
 // 	selectCohorts.innerHTML += "<option value='" + idCohort + "'>" + idCohort + "</option>";
 //_______________________________PARA FILTRAR SEGUN SEDE LIM__________________________________________________//
 
 //FUNCION PARA CREAR EL SELECT DE SEDES CON UN TEMPLATE
 //__________________________funcion fetch para jalar la data de cohorts_______________________________________//
-const viewListCohorts = () => {
+let viewListCohorts = () => {
 	// selectCohorts.innerHTML = "";
 	let cohortsJsonArr = [];
 	fetch(urlCohorts)
-		.then(res => res.json())
+		.then(respuesta => respuesta.json())
 		.then(cohorts => {
 			// console.log(cohorts);//cohorts es el array que contiene a todos los cohort
-			const arrayCohorts = cohorts.filter((elementCohorts) => {
-				let idCohort = elementCohorts.id;
-				if ((idCohort).includes('lim', 0) === true) {
-					//aqui tengo que poner en vez de lim, la etiqueta que va a ser seleccionada en el select
-					selectCohorts.innerHTML += "<option value='" + idCohort + "'>" + idCohort + "</option>";
-					console.log(idCohort);
-					cohortsJsonArr.push(elementCohorts);
-				};
-			});
+
+			selectSede.addEventListener('change', () => {
+				let opcionSeleccionadaDeSede = event.target.options[selectSede.selectedIndex];
+				console.log(opcionSeleccionadaDeSede.value);
+
+				// let deleteCohorts = () => {
+				// 	let listOfCohorts = document.getElementById('selectCohorts');
+				// 	let numberOfCohorts = listOfCohorts.length;
+				// 	for (i = 0; numberOfCohorts > i; i++) {
+				// 		listOfCohorts.remove(0);
+				// 	}
+
+					let arrayDeCohorts = cohorts.filter((elementCohorts) => {
+						let idCohort = elementCohorts.id;
+						(elementCohorts.id).includes(opcionSeleccionadaDeSede.value);
+						
+						if ((idCohort).includes(opcionSeleccionadaDeSede.value, 0) === true) {
+							//aqui tengo que poner en vez de lim, la etiqueta que va a ser seleccionada en el select
+							selectCohorts.innerHTML += "<option value='" + idCohort + "'>" + idCohort + "</option>";
+						}
+						// deleteCohorts();
+					})
+				// }
+			})
+
+
+			// console.log(Array.isArray(arrayDeCohorts));
+			console.log(cohorts);
+
+
 			//______________Detectar el nombre que el usuario desea buscar_______________________________________________//
-				inputName.addEventListener('input', (event) => {
-				const valorEscrito = event.target.value;
-				const arrayNombresFiltrado = cohorts.filter((cohort) => {
-					return cohort.toUpperCase().indexOf(valorEscrito.toUpperCase()) !== -1;
-				});
-				// console.log(arrayNombresFiltrado);
-			});
+			// 	inputName.addEventListener('input', (event) => {
+			// 	const valorEscrito = event.target.value;
+			// 	const arrayNombresFiltrado = cohorts.filter((cohort) => {
+			// 		return cohort.toUpperCase().indexOf(valorEscrito.toUpperCase()) !== -1;
+			// 	});
+			// 	// console.log(arrayNombresFiltrado);
+			// });
 			//______________Detectar el nombre que el usuario desea buscar_______________________________________________//
-		});
+		})
 	//__________________________funcion fetch para jalar la data de cohorts_______________________________________//
 }
+
 
 viewListCohorts();
 
@@ -67,7 +92,7 @@ const viewListUsers = () => {
 		});
 }
 
-viewListUsers();
+// viewListUsers();
 //llamando al json de Progress
 const viewListProgress = () => {
 	selectProgress.innerHTML = "";
@@ -79,4 +104,4 @@ const viewListProgress = () => {
 
 		});
 }
-viewListProgress();
+// viewListProgress();
