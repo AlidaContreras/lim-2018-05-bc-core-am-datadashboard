08f54c8aa7a62caa71ddb87ee60d9ab34bbdc279
AlidaContreras <alidacontreras01@gmail.com>
2018-07-09 00:53:26 -0500

    Ya filtra correctamente y devuelve el array de usuarios en 726

--- src/main.js
+++ src/main.js
@@ -5,26 +5,16 @@ const urlProgress = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
 const selectSede = document.getElementById('selectSede');
 const inputName = document.getElementById('inputName');
 
-
-// const arraySedes = ['LIM', 'GDL','AQP','SCL','SPL','CDMX'];
-//_______________________________PARA FILTRAR SEGUN SEDE LIM__________________________________________________//
-// 	const arrayDeCohorts = cohorts.filter((elementCohorts) => {
-// 	let idCohort = elementCohorts.id ;
-//	if ((idCohort).includes('lim', 0) === true) {
-// 	//aqui tengo que poner en vez de lim, la etiqueta que va a ser seleccionada en el select
-// 	selectCohorts.innerHTML += "<option value='" + idCohort + "'>" + idCohort + "</option>";
-//_______________________________PARA FILTRAR SEGUN SEDE LIM__________________________________________________//
-
-//FUNCION PARA CREAR EL SELECT DE SEDES CON UN TEMPLATE
 //__________________________funcion fetch para jalar la data de cohorts_______________________________________//
-let viewListCohorts = () => {
+
+
+const viewListCohorts = () => {
 	// selectCohorts.innerHTML = "";
-	let cohortsJsonArr = [];
+	const cohortsJsonArr = [];
 	fetch(urlCohorts)
 		.then(respuesta => respuesta.json())
 		.then(cohorts => {
 			// console.log(cohorts);//cohorts es el array que contiene a todos los cohort
-
 			selectSede.addEventListener('change', () => {
 				let opcionSeleccionadaDeSede = event.target.options[selectSede.selectedIndex];
 				console.log(opcionSeleccionadaDeSede.value);
@@ -36,7 +26,7 @@ let viewListCohorts = () => {
 				// 		listOfCohorts.remove(0);
 				// 	}
 
-					let arrayDeCohorts = cohorts.filter((elementCohorts) => {
+					cohorts.filter((elementCohorts) => {
 						let idCohort = elementCohorts.id;
 						(elementCohorts.id).includes(opcionSeleccionadaDeSede.value);
 						
@@ -48,21 +38,7 @@ let viewListCohorts = () => {
 					})
 				// }
 			})
-
-
 			// console.log(Array.isArray(arrayDeCohorts));
-			console.log(cohorts);
-
-
-			//______________Detectar el nombre que el usuario desea buscar_______________________________________________//
-			// 	inputName.addEventListener('input', (event) => {
-			// 	const valorEscrito = event.target.value;
-			// 	const arrayNombresFiltrado = cohorts.filter((cohort) => {
-			// 		return cohort.toUpperCase().indexOf(valorEscrito.toUpperCase()) !== -1;
-			// 	});
-			// 	// console.log(arrayNombresFiltrado);
-			// });
-			//______________Detectar el nombre que el usuario desea buscar_______________________________________________//
 		})
 	//__________________________funcion fetch para jalar la data de cohorts_______________________________________//
 }
@@ -78,21 +54,40 @@ const viewListUsers = () => {
 		.then(res => res.json())
 		.then(users => {
 			// console.log(usersJson);//UsersJson es el Json sin parsear
-			users.map((users) => {
-				let roleUsers = users.role;
-				let nameUsers = users.name;
+		 const arrayDeAlumnasFiltadas	= users.filter((user) => {
+				let roleUsers = user.role;
+				let nameUsers = user.name;
 				if (((roleUsers) === 'student') === true) {
 					tableUsers.innerHTML += `
 						<tr>
 						<td> ${nameUsers}</td>
 						</tr>
 						`
-				}
-			});
+					return nameUsers ;
+				}// console.log(Array.isArray(usersEstudiantes));
+			})
+			console.log(arrayDeAlumnasFiltadas.length);
+							
+				//______________Detectar el nombre que el usuario desea buscar_______________________________________________//
+				inputName.addEventListener('input', (event) => {
+				const valorEscrito = event.target.value;
+				console.log(valorEscrito);
+				const alumnaBuscada = arrayDeAlumnasFiltadas.filter((nombreDeAlumna)=>{
+					return (nombreDeAlumna.toUpperCase().indexOf(valorEscrito.toUpperCase()) !==-1);
+				});
+				console.log(alumnaBuscada);
+				// alumnaBuscada.forEach(filtrada => {
+				// 	console.log (filtrada);
+					
+				// });
+				
+				console.log(alumnaBuscada);
+				//______________Detectar el nombre que el usuario desea buscar_______________________________________________//
+			})
 		});
 }
+viewListUsers();
 
-// viewListUsers();
 //llamando al json de Progress
 const viewListProgress = () => {
 	selectProgress.innerHTML = "";
