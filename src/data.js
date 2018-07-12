b7f918289366e657cfda7b06cb0f0317c5839776
AlidaContreras <alidacontreras01@gmail.com>
2018-07-12 10:38:09 -0500

    La función 1 esta a un 20%

--- src/data.js
+++ src/data.js
@@ -1,42 +1,123 @@
-window.computeUsersStats = (users, progress, courses,cohorts) => {
-     cohorts = cohorts;
-    progress = progress;
-    // courses = courses;
+window.computeUsersStats = (users, progress, courses) => {
+	let dataUsers = users;
+	let dataProgress = progress;
 
-    //valor de retorno
-    // usersWithStats: {
-    //     stats:{
-    //         percent:,//% completitud general del usuario de todos los cursos
-    //         exercises:{
-    //             total:,//total ejercicios autocorregidos
-    //             completed:,//numero de ejercicios autocorregidos completados
-    //             percent:,//%de ejercicios autocorregidos completados
-    //         },
-    //         reads:{
-    //             total:,//total de lecturas presentes
-    //             completed:,//numero de lecturas completadas por el usuario
-    //             percent:,//%de lecturas completadas
-    //         },
-    //         quizzes:{
-    //             total:,//numero total quizzes presentes
-    //             completed:,//quizzes completados
-    //             percent:,//%quizzes completados
-    //             scoreSum:,//suma de todas puntuaciones de los quizzes completos
-    //             scoreAvg:,//promedio de puntuacion quizzes
-    //         }
+	//1. vamos a filtrar a las alumnas que tiene su role como estudiante
+	let students = dataUsers.filter(user => user.role === 'student');
+	//2.conexion entre mis users y el progress
+	students = students.map(user => {//en students se va a chancar un array con estudiantes
+		//que tienen definido en el rol a 'students'
+		//userProgress une a progress y users; userProgress=id del user buscando en progress
+		const userProgress = dataProgress[user.id];//userProgress une a progress y users; userProgress=id del user buscando en progress
+		//para calcular el percent, exercises, reads y quizzes se hace una funcion general callback
+		//porque la forma de obtenerlos es similar, solo cambiariamos los parametros->calcularStats()
+		let percent = calcularStats(userProgress);
+		let exercises = calcularStats(userProgress, 'practice');
+		let reads = calcularStats(userProgress,'read');
+		let quizzes = calcularStats(userProgress, 'quiz');
+		return ({//aqui creo el objeto stats que me va a devolver los datos requeridos juntos
+			id: user.id,
+			name: user.name.toUpperCase(),
+			stats: {
+				percent,//1.
+				percentBycourses,//2
+				exercises,//3
+				reads,//4
+				quizzes//5
+			},
+		})
+	})
+	//3. 
+	//funcion para calcular el % completitud genral
+	const calcuPercent = (user) => {
+		let count = 0;//acumular todo el puntaje de %
+		let percentDeCursos = [];
+		courses.map(course => {
+			if (user[course]) {
+				count += user[course]['percent'];
+				percentDeCursos.push(user[courses]['percent']);
+			}
+		});
+		return {
+			percent: count / courses.length,
+			percentDeCursos
+		}
+	}
 
-    //     }
-    // }
+
+	//funcion para calcular la data de los ejercicios, lecturas y quizzes
+	const calcularStats = (user, type) => {
+
+		let [completed, total, scoresum, totalCompletedQuizzes] = [0, 0, 0, 0]
+		let complitedDeCursos = courses.map(course => {
+			let complitedDelCurso = 0;
+			if (user.hasOwnProperty(course)) { // si(el objeto(user).contiene(la propiedad(course)))
+				let units = Object.value(user[course]['units']);
+				units.map(unit => {
+					let parts = Object.value(unit['parts']).filter(part => part.type)
+					const calcuData = values => {
+						total++;
+						completed += value;
+						complitedDelCurso += value;
+					},
+					switch (type) {
+						case 'practica':
+							parts
+
+							break;
+
+						default:
+							break;
+					}
+
+				})
+
+			}
+			else{
+				console.log ('EL USUARIO NO TIENE CURSOS')
+			}
+		})
+		let units = Object.value
+	}
+
+
+	// //valor de retorno
+	// usersWithStats: {
+	//     stats:{
+	//         percent:,//% completitud general del usuario de todos los cursos
+	//         exercises:{
+	//             total:,//total ejercicios autocorregidos
+	//             completed:,//numero de ejercicios autocorregidos completados
+	//             percent:,//%de ejercicios autocorregidos completados
+	//         },
+	//         reads:{
+	//             total:,//total de lecturas presentes
+	//             completed:,//numero de lecturas completadas por el usuario
+	//             percent:,//%de lecturas completadas
+	//         },
+	//         quizzes:{
+	//             total:),//numero total quizzes presentes
+	//             completed:,//quizzes completados
+	//             percent:,//%quizzes completados
+	//             scoreSum:,//suma de todas puntuaciones de los quizzes completos
+	//             scoreAvg:,//promedio de puntuacion quizzes
+	//         }
+
+	//     }
+	// }
 }
-computeUsersStats();
-// window.sortUsers = (parametros)=>{
 
-// }
-// window.filterUsers = (parametros)=>{
+window.sortUsers = (parametros) => {
 
-// }
-window.processCohortData = (option) => {
-   
+}
+window.filterUsers = (parametros) => {
 
+}
+window.processCohortData = (options) => {
 
+	let courses = Object.keys(options.cohort.coursesIndex);
+	console.log(courses);
+	let users = computeUsersStats(options.cohortData.users, options.cohortData.progress, courses);
+	// users = sortUsers(users,orderBy,orderDirection);
+	// search ? users = filterUsers(users,search) : null;
 }
--- src/main.js
+++ src/main.js
@@ -22,31 +22,36 @@ const viewListCohorts = () => {
 	fetch(urlCohorts)
 		.then(respuesta => respuesta.json())
 		.then(cohorts => {//cohorts es el documento parseado en este caso es un array de objetos
-			options.cohort = cohorts;
+			
+			selectSede.addEventListener('change', () => {
+				let opcionSeleccionadaDeSede = event.target.options[selectSede.selectedIndex];
+				console.log(opcionSeleccionadaDeSede.value);
+				selectCohorts.innerHTML = "";
+				let cohortsLima = cohorts.filter((elementCohorts) => {
+					let idCohort = elementCohorts.id;
+					(elementCohorts.id).includes(opcionSeleccionadaDeSede.value);
+					if ((idCohort).includes(opcionSeleccionadaDeSede.value, 0) === true) {
+						return selectCohorts.innerHTML += "<option value='" + idCohort + "'>" + idCohort + "</option>";
+					}
+				})
+					let cohort = cohortsLima.filter((elCohort)=>{
+					if	(elCohort.id === "lim-2018-03-pre-core-pw"){
+						return (elCohort);
+					}
+				})
+				console.log(cohort[0])
+				options.cohort = cohort[0];
+			})
 		})
 }
 viewListCohorts();
-
-selectSede.addEventListener('change', () => {
-		let opcionSeleccionadaDeSede = event.target.options[selectSede.selectedIndex];
-		console.log(opcionSeleccionadaDeSede.value);
-		selectCohorts.innerHTML = "";
-			let cohortsLima = options.cohort.filter((elementCohorts) => {
-			let idCohort = elementCohorts.id;
-			(elementCohorts.id).includes(opcionSeleccionadaDeSede.value);
-			if ((idCohort).includes(opcionSeleccionadaDeSede.value, 0) === true) {
-				selectCohorts.innerHTML += "<option value='" + idCohort + "'>" + idCohort + "</option>";
-			}
-		})
-	})
-
 
 const viewListProgress = () => {
 	// selectProgress.innerHTML = "";
 	fetch(urlProgress)
 		.then(res => res.json())
 		.then(progress => {//progress es el documento parseado en este caso es un objeto de objetos
-			option.cohortData.progress = progress;
+			options.cohortData.progress = progress;
 		});
 }
 
@@ -55,64 +60,54 @@ const viewListUsers = () => {
 	fetch(urlUsers)
 		.then(res => res.json())
 		.then(users => {//users es el documento parseado en este caso es un array de objetos
-			option.cohortData.users = users;
+			options.cohortData.users = users;
 			viewListProgress()
-
-			//users es el array del Json parseado
-			// const arrayDeNombresFiltados=users.filter((user) => {
-			// 	let roleUsers = user.role;
-			//  	let		nameUsers = user.name;
-			// 	if (((roleUsers) === 'student') === true) {
-			// 		tableUsers.innerHTML += `
-			// 			<tr>
-			// 			<td> ${nameUsers}</td>
-			// 			</tr>
-			// 			`
-			// 		return nameUsers;
-			// 	}
-			// });
-			//______________Detectar el nombre que el usuario desea buscar_______________________________________________//
-			// inputName.addEventListener('input', (event) => {
-			// 	const valorEscrito = event.target.value;
-			// 	console.log(valorEscrito);
-			// 	// console.log(arrayDeNombresFiltados);
-			// 	const alumnaBuscada = arrayDeNombresFiltados.filter((user) => {
-			// 		const nombres = user['name'];
-			// 		return (nombres.toUpperCase().indexOf(valorEscrito.toUpperCase()) !== -1);
-			// 	});
-			// 	const ulElemento = document.getElementById('listaAlumnaBuscada');
-			// 	ulElemento.innerHTML = "";
-			// 	alumnaBuscada.forEach((user) => {
-			// 	ulElemento.innerHTML += `
-			// 													<li>${user['name']}</li>
-			// 													`
-			// 	});
-			// })
-			//______________Detectar el nombre que el usuario desea buscar_______________________________________________//
-		});
+		})
 }
 
-selectCohorts.addEventListener('change',()=>{
-	if (selectCohorts.value === 'lim-2018-03-pre-core-pw'){
-		console.log ('hola')
-	}
-	else{
-		console.log ('DATA NO EXISTENTE')
+selectCohorts.addEventListener('change', () => {
+	if (selectCohorts.value === 'lim-2018-03-pre-core-pw') {
+		viewListUsers();
+		processCohortData(options)
+		
+
+		}
+	else {
+		console.log('DATA NO EXISTENTE')
 	}
 
 
 })
 
+// // ESTO ME DEBE PINTAR EN PANTALLA A LAS ESTUDIANTES(SOLO NOMBRES)
+// const arrayDeNombresFiltados = users.filter((user) => {
+// 	let roleUsers = user.role;
+// 	let nameUsers = user.name;
+// 	if (((roleUsers) === 'student') === true) {
+// 		tableUsers.innerHTML += `
+// 			<tr>
+// 			<td> ${nameUsers}</td>
+// 			</tr>
+// 			`
+// 		return nameUsers;
+// 	}
+// });
+// //______________Detectar el nombre que el usuario desea buscar_______________________________________________//
+// inputName.addEventListener('input', (event) => {
+// 	const valorEscrito = event.target.value;
+// 	console.log(valorEscrito);
+// 	// console.log(arrayDeNombresFiltados);
+// 	const alumnaBuscada = arrayDeNombresFiltados.filter((user) => {
+// 		const nombres = user['name'];
+// 		return (nombres.toUpperCase().indexOf(valorEscrito.toUpperCase()) !== -1);
+// 	});
+// 	const ulElemento = document.getElementById('listaAlumnaBuscada');
+// 	ulElemento.innerHTML = "";
+// 	alumnaBuscada.forEach((user) => {
+// 		ulElemento.innerHTML += `
+// 													<li>${user['name']}</li>
+// 													`
+// 	});
+// })
 
 
-// viewListUsers();
-//__________________________FIN  de la funcion fetch para jalar la data de users_______________________________________//
-
-//__________________________INICIO de la funcion fetch para jalar la data del progress_______________________________________//
-
-// viewListProgress();
-//__________________________FIN de la funcion fetch para jalar la data del progress_______________________________________//
-// Promise.all([viewListCohorts,viewListUsers,viewListProgress])
-// 	.then((values) => {
-// 		userStats = window.getComputedStyle(cohorts,users,progress);
-// 	})
